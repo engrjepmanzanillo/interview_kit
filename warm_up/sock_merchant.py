641076c7b8129c14bb7bb2076b3f0228d3754d1f
engrjepmanzanillo <engr.jeffmanzanillo@gmail.com>
2019-10-14 07:04:47 +0800

    initial commit

---
+++ warm_up/sock_merchant.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    #complete_socks = []
+    print(ar)
+    return ar
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    #n = 9
+
+    #ar = list(map(int, input().rstrip().split()))
+    ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+    print(ar)
+    #sockMerchant(n, ar)
+
+    #fptr.write(str(result) + '\n')
+
+    #fptr.close()
